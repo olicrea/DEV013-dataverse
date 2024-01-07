b6bf1a91429c6584ec663138c0926b6da686e3ef
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2024-01-07 00:59:47 -0500

    Se agrego una renderización defiltro a modo de prueba

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { ordenAlfaAsc, ordenYearsAsc, ordenAlfaDesc, ordenYearsDes } from './dataFunctions.js';
-import { renderItems } from './view.js';
+import { ordenAlfaAsc, ordenYearsAsc, ordenAlfaDesc, ordenYearsDes, filtrarThriller } from './dataFunctions.js';
+import { renderItems,renderFiltThriller } from './view.js';
 
 import data from './data/dataset.js';
 
 //console.log(example, renderItems(data), data);
-
+  
 const alfaAsc = ordenAlfaAsc(data);
 console.log("estoy ordenando A-Z", alfaAsc);
 
@@ -17,7 +17,8 @@ console.log("estoy ordenando por años de manera ascendente", yearsAsc);
 const yearsDes = ordenYearsDes(data);
 console.log("estoy ordenando por años de manera descendente", yearsDes);
 
-
+const filtThriller = renderFiltThriller(data);
+console.log("estoy filtrando por género Thriller", filtThriller);
 
 const renderedHTML = renderItems(data);
 console.log(renderedHTML);
--- src/view.js
+++ src/view.js
@@ -1,3 +1,5 @@
+import data from "./data/dataset";
+
 export const renderItems = (data) => {
   //console.log(data)
   let html = '';
@@ -8,11 +10,36 @@ export const renderItems = (data) => {
                 <dt></dt><dd itemprop="name">${film.name}</dd>
                 <dt></dt><dd itemprop="shortDescription">${film.shortDescription}</dd>
                 <dt></dt><dd itemprop="yearMovie">${film.facts.yearMovie}</dd>
-              
+
               </dl>
               </li>`
   });
 
   html = '<ul>' + html + '</ul>';
   return html;
+};
+
+export const renderFiltThriller = (data) => {
+  
+  //obtener un nuevo renderizado con películas de género de Thriller 
+  //1o búsqueda de coincidencias en genero
+  //2o agregarle estructura microdatos en HTML
+  //3o retornar el arreglo
+  let html = '';
+  data.forEach(function (film) { 
+    if(film.facts.genreMovie === "Thriller"){
+      //arrayThriller.push(film);
+      html += `<li itemscope itemtype="movie">
+                  <dl>
+                    <img src="${film.imageUrl}" alt=${film.name} />
+                    <dt></dt><dd itemprop="name">${film.name}</dd>
+                    <dt></dt><dd itemprop="shortDescription">${film.shortDescription}</dd>
+                    <dt></dt><dd itemprop="yearMovie">${film.facts.yearMovie}</dd>
+
+                  </dl>
+                </li>`
+    }
+  });
+  html = '<ul>' + html + '</ul>';
+  return html
 };
